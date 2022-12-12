8d93609c41b495d7424f3b6fd499bd51daf22692
Donatas Uziela <4706298+DonatasUziela@users.noreply.github.com>
2022-12-12 16:18:45 +0200

    `maximum-difference-between-node-and-ancestor`

--- problems/maximum-difference-between-node-and-ancestor/index.mjs
+++ problems/maximum-difference-between-node-and-ancestor/index.mjs
@@ -1,12 +1,51 @@
 // https://leetcode.com/problems/maximum-difference-between-node-and-ancestor/
 // node problems/maximum-difference-between-node-and-ancestor/index.mjs
 
-import { TreeNode } from "../../utils/TreeNode.mjs";
+import { expect } from "chai";
+import { TreeNode, postOrder } from "../../utils/TreeNode.mjs";
+import { testData } from './testData.mjs'
+
+const isDefined = (value) => value !== undefined && value !== null;
+
+/**
+ * @param {TreeNode} root
+ */
+ const getSubtree = ({ val, left, right }, subTrees = []) => {
+    const subTree = [];
+
+    const l = isDefined(left) ? getSubtree(left, subTrees) : null;
+    const r = isDefined(right) ? getSubtree(right, subTrees) : null;
+
+    if (l) subTree.push(...l)
+    if (r) subTree.push(...r)
+
+    subTree.push(val);
+    subTrees.push(subTree);
+
+    return subTree
+}
+
+const reduceDiff = (result, item, array) => {
+
+} 
 
 /**
  * @param {TreeNode} root
  * @return {number}
  */
- var maxAncestorDiff = function(root) {
-    
+var maxAncestorDiff = function(root) {
+    const subtrees = [];
+    getSubtree(root, subtrees);
+    const result = subtrees
+        .map(s => s.slice(0, -1))
+        .filter(s => s.length)
+        .map(s => s.reduce((reduceDiff, [])))
+    console.log({ result })
+    return Math.max(result)
 };
+
+testData.forEach(({ root, expectedResult }) => {
+    const node = postOrder(root);
+    console.log(node)
+    expect(maxAncestorDiff(node)).to.equal(expectedResult);
+})
---
+++ problems/maximum-difference-between-node-and-ancestor/testData.mjs
@@ -0,0 +1,14 @@
+export const testData = [
+    {
+        root: [null, 8, 3, 10, 1, 6, null, 14, null, null, 4, 7, 13],
+        expectedResult: 7
+    },
+    // {
+    //     root: [8, 3, 10, 1, 6, null, 14, null, null, 4, 7, null, null, 13],
+    //     expectedResult: 7
+    // },
+    {
+        root: [null, 1, null, 2, null, 0, 3],
+        expectedResult: 3
+    }
+]
